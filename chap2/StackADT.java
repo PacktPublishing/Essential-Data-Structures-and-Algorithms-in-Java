725c67cfceda7d3c5d2bf5813dc3b9f7f0c044cc
jsu800 <jsu800@gmail.com>
2025-10-05 19:09:02 -0700

    adding stack adt and concrete implementation for array

---
+++ chap2/StackADT.java
@@ -0,0 +1,19 @@
+public interface StackADT<T> {  
+    // Push an element onto the top of the stack
+    void push(T element);
+
+    // Remove the top element from the stack
+    T pop();
+
+    // Return a copy of the top element without removing it
+    T peek();
+
+    // Return the number of elements in the stack
+    int size();
+
+    // Return whether the stack has any elements in it
+    boolean isEmpty();
+
+    // Resize the stack when it is at capacity
+    void resize();
+}

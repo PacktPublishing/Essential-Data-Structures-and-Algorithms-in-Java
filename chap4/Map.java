fe12fce0b0986a327c565174317b1d7eb0c0a1d2
jsu800 <jsu800@gmail.com>
2025-11-23 22:07:33 -0800

    adding map adt

---
+++ chap4/Map.java
@@ -0,0 +1,18 @@
+public interface Map<K,V> {
+    
+    // Insert a new key‑value pair or overwrite the value for an existing key.
+    V put(K key, V value);              
+    
+    // Retrieve the value associated with key k.
+    V get(Object key);
+
+    // Delete the entry identified by key k.
+    V remove(Object key);
+
+    // Checks whether the stack contains any elements
+    boolean isEmpty();
+
+    // Number of elements currently stored
+    int size();
+
+}

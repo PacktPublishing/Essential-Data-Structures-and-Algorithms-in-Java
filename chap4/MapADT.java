7da6cbde9c6091200a7980892c580bb73d48b576
jsu800 <jsu800@gmail.com>
2026-06-10 01:01:46 -0700

    Corrected comments and also K type

--- chap4/MapADT.java
+++ chap4/MapADT.java
@@ -1,18 +1,19 @@
 public interface Map<K,V> {
     
-    // Insert a new key‑value pair or overwrite the value for an existing key.
-    V put(K key, V value);              
-    
+    // Insert a new key-value pair or overwrite the value for an existing key.
+    V put(K key, V value);
+   
     // Retrieve the value associated with key k.
-    V get(Object key);
+    V get(K key);
 
     // Delete the entry identified by key k.
-    V remove(Object key);
+    V remove(K key);
 
-    // Checks whether the stack contains any elements
+    // Checks whether the map contains any elements
     boolean isEmpty();
 
     // Number of elements currently stored
     int size();
 
 }
+
